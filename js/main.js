bd6f3e9e58b5b49900e4a7f63a64ebc3e94d4d21
SolihovAbdulloh <abdullasolikh@mail.com>
2024-09-25 15:43:45 +0500

    finish figmaaaaa

---
+++ js/main.js
@@ -0,0 +1,59 @@
+// 1-masala
+
+// function vaporcode(str) {
+//   let str1 = str.toUpperCase();
+
+//     let str2 = ""
+
+// for(let i = 0 ; i < str1.length ; i++){
+//     if(str1[i] !== " "){
+
+//         str2 += str1[i] + "  "
+//     }
+
+// }
+
+//   return str2
+
+// }
+// let str = "Helloworld";
+
+// console.log(vaporcode(str));
+
+// function Sum(number) {
+//   let arr = String(number).split("");
+
+//   let sum = 0;
+
+//   for (let i in arr) {
+//     sum += +i;
+//   }
+//   return sum;
+// }
+
+// let a = 16;
+
+// console.log(Sum(a));
+
+// Futbol jamoamiz chempionatni yakunladi.
+
+// Jamoamizning o'yin natijalari qatorlar to'plamida qayd etilgan. Har bir o'yin formatidagi satr bilan 
+
+// ifodalanadi "x:y", bu erda xjamoamizning ochkolari va yraqiblarimizning ochkolari.
+
+// Masalan: ["3:1", "2:2", "0:1", ...]
+
+// Har bir o'yin uchun ochkolar quyidagicha taqsimlanadi:
+
+// agar x > y bo'lsa: 3 ball (g'alaba)
+// agar x < y: 0 ball (yo'qotish)
+// agar x = y bo'lsa: 1 ball (bo'g'in)
+// xBiz ushbu to'plamni oladigan va yuqorida keltirilgan qoidalar bo'yicha jamoamiz chempionatda olgan ( ) ochkolar sonini qaytaradigan funksiya yozishimiz kerak .
+
+// Eslatmalar:
+
+// jamoamiz chempionatda doim 10ta o'yin o'tkazadi
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+
